23246564ba474ca7f2d1131a52b4d8c322156668
Tristan <l.tristan.wong@gmail.com>
2023-11-13 01:12:45 +0000

    Added ShiftRows method

--- AES/Program.cs
+++ AES/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System.Diagnostics.Metrics;
+using System.Reflection;
 using System.Text;
 
 namespace AES
 {
-    class GaloisField
+    internal class GaloisField
     {
-        static byte[] logTable = new byte[]
+        private static byte[] logTable = new byte[]
 {
             0x00, 0xff, 0xc8, 0x08, 0x91, 0x10, 0xd0, 0x36,
             0x5a, 0x3e, 0xd8, 0x43, 0x99, 0x77, 0xfe, 0x18,
@@ -40,7 +41,7 @@ namespace AES
             0x3b, 0x52, 0x6f, 0xf6, 0x2e, 0x89, 0xf7, 0xc0,
             0x68, 0x1b, 0x64, 0x04, 0x06, 0xbf, 0x83, 0x38
 };
-        static byte[] antilogTable = new byte[]
+        private static byte[] antilogTable = new byte[]
         {
             0x01, 0xe5, 0x4c, 0xb5, 0xfb, 0x9f, 0xfc, 0x12,
             0x03, 0x34, 0xd4, 0xc4, 0x16, 0xba, 0x1f, 0x36,
@@ -106,14 +107,14 @@ namespace AES
             return c;
         }
     }
-    class KeyExpansion
+    internal class KeyExpansion
     {
-        string key;
-        byte[] keyBytes;
+        private string key;
+        private byte[] keyBytes;
         internal byte[] ExpandedKey = new byte[176];
-        byte[] word = new byte[4];
-        byte byteCounter = 16;
-        byte i = 1;
+        private byte[] word = new byte[4];
+        private byte byteCounter = 16;
+        private byte i = 1;
         GaloisField GF = new GaloisField();
         internal KeyExpansion(string originalKey)
         {
@@ -173,21 +174,23 @@ namespace AES
             return x;
         }
     }
-    class Block
+    internal class Block
     {
-        private int size = 4;
+        internal int Size { get; }
         private byte[,] block;
-        public byte this[int i, int j]
+        internal byte this[int i, int j]
         {
             get { return block[i, j]; }
+            set { block[i, j] = value; }
         }
-        public Block(byte[] plaintextBytes)
+        internal Block(byte[] plaintextBytes)
         {
+            Size = 4;
             block = new byte[4, 4];
             int index = 0; // check if block can't be filled anymore
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < Size; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < Size; j++)
                 {
                     if (index < plaintextBytes.Length)
                     {
@@ -200,7 +203,86 @@ namespace AES
                     }
                 }
             }
-            Console.WriteLine(block[0, 1]);
+        }
+    }
+    internal class Encryption
+    {
+        private int counter;
+        internal Block AddRoundKey(Block block, byte[] expandedKey)
+        {
+            counter = 0;
+            for (int i = 0; i < block.Size; i++)
+            {
+                for (int j = 0; j < block.Size; j++)
+                {
+                    block[i, j] = (byte)(block[i, j] ^ expandedKey[counter]);
+                    counter++;
+                }
+            }
+            return block;
+        }
+        internal byte SubByte(byte b)
+        {
+            byte[,] sBox = new byte[,]
+            {
+                // 0    1     2     3     4     5     6     7     8     9     A     B     C     D     E     F
+                {0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76}, // 0
+                {0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0}, // 1
+                {0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15}, // 2
+                {0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75}, // 3
+                {0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84}, // 4
+                {0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF}, // 5
+                {0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8}, // 6
+                {0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2}, // 7
+                {0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73}, // 8
+                {0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB}, // 9
+                {0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79}, // A
+                {0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08}, // B
+                {0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A}, // C
+                {0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E}, // D
+                {0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF}, // E
+                {0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}  // F
+            };
+            // Extract the row and column indices from the input byte
+            int row = (b >> 4) & 0x0F;    // First 4 bits represent the row
+            int col = b & 0x0F;           // Last 4 bits represent the column
+            return sBox[row, col];
+        }
+        internal Block ShiftRows(Block block, int n)
+        {
+            if (n == 4)
+            {
+                return block;
+            }
+            byte[] row = new byte[block.Size];
+            byte[] temp = new byte[row.Length];
+            // Define a row
+            for (int i = 0; i < row.Length; i++)
+            {
+                row[i] = block[n, i];
+            }
+            // Copy the first 'n' elements to the temporary array.
+            for (int i = 0; i < n; i++)
+            {
+                temp[i] = row[i];
+            }
+            // Shift the remaining elements 'n' positions to the left.
+            for (int i = 0; i < row.Length - n; i++)
+            {
+                row[i] = row[i + n];
+            }
+            // Copy the temporary elements back to the original array at the end.
+            for (int i = 0; i < n; i++)
+            {
+                row[row.Length - n + i] = temp[i];
+            }
+            // Put row back into block
+            for (int i = 0; i < row.Length; i++)
+            {
+                block[n, i] = row[i];
+            }
+            n += 1;
+            return ShiftRows(block, n);
         }
     }
     internal class Program
@@ -209,8 +291,8 @@ namespace AES
         {
             Console.WriteLine("Hello, World!");
 
-            /*string originalKey = "0000000000000000"; // 16 byte key
-            KeyExpansion key = new KeyExpansion(originalKey);*/
+            string originalKey = "0000000000000000"; // 16 byte key
+            KeyExpansion key = new KeyExpansion(originalKey);
 
             string plaintext = "Hello, World! Goodbye, World!";
             byte[] plaintextBytes = Encoding.ASCII.GetBytes(plaintext); // ASCII, each character is represented as a byte
@@ -220,17 +302,29 @@ namespace AES
             Block[] plaintextBlocks = new Block[numberOfBlocks];
 
             int counter = 0;
-            for (int i = 0; i < plaintextBlocks.Length; i++)
+            for (int i = 0; i < plaintextBlocks.Length; i++) // Fill plaintextBlocks with plaintext
             {
                 plaintextBytesSliced = new byte[plaintextBytes.Length - counter];
                 plaintextBytesSliced = plaintextBytes[counter..];
                 plaintextBlocks[i] = new Block(plaintextBytesSliced);
                 counter += 16;
             }
+            // Console.WriteLine(plaintextBlocks[0][1, 2]);
 
-            Console.WriteLine(plaintextBlocks[0][0, 1]);
-            Console.WriteLine(plaintextBlocks[1][0, 1]);
+            Encryption encrypt = new Encryption();
+            plaintextBlocks[0] = encrypt.AddRoundKey(plaintextBlocks[0], key.ExpandedKey); // XOR block 1 with round key 1
 
+            for (int i = 0; i < plaintextBlocks[0].Size; i++) // Sub all bytes in block 1
+            {
+                for (int j = 0; j < plaintextBlocks[0].Size; j++)
+                {
+                    plaintextBlocks[0][i,j] = encrypt.SubByte(plaintextBlocks[0][i, j]);
+                }
+            }
+
+            plaintextBlocks[0] = encrypt.ShiftRows(plaintextBlocks[0], 0); // Shift rows
+
+    
             Console.ReadLine();
         }
     }
