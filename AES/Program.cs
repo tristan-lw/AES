c10c3b1af597cefa43cda469369d22c53cabf909
Tristan <l.tristan.wong@gmail.com>
2023-12-04 11:49:12 +0000

    It works with 1 block!!

--- AES/Program.cs
+++ AES/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System.Data;
+using System.Diagnostics.Metrics;
 using System.IO;
 using System.Reflection;
 using System.Text;
@@ -377,33 +378,6 @@ namespace AES
             }
             return block;
         }
-        internal byte SubByte(byte b)
-        {
-            byte[,] sBox = new byte[,]
-            {
-                // 0    1     2     3     4     5     6     7     8     9     A     B     C     D     E     F
-                {0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76}, // 0
-                {0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0}, // 1
-                {0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15}, // 2
-                {0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75}, // 3
-                {0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84}, // 4
-                {0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF}, // 5
-                {0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8}, // 6
-                {0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2}, // 7
-                {0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73}, // 8
-                {0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB}, // 9
-                {0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79}, // A
-                {0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08}, // B
-                {0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A}, // C
-                {0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E}, // D
-                {0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF}, // E
-                {0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}  // F
-            };
-            // Extract the row and column indices from the input byte
-            int row = (b >> 4) & 0x0F;    // First 4 bits represent the row
-            int col = b & 0x0F;           // Last 4 bits represent the column
-            return sBox[row, col];
-        }
         internal Block ShiftRows(Block block, int n)
         {
             if (n == 4)
@@ -435,7 +409,7 @@ namespace AES
             n += 1;
             return ShiftRows(block, n);
         }
-        internal Block MixColumns(Block block)
+        internal byte[] MixColumn(byte[] row)
         {
             // Step 1: take column from block
             // Step 2: Multiply
@@ -455,22 +429,24 @@ namespace AES
             // 3 * 1 +
             // 4 * 1
 
-            GaloisField GF = new GaloisField();
+            byte[] a = new byte[4];
+            byte[] b = new byte[4];
+            byte c;
+            byte h;
 
-            
-            for (int i = 0; i < block.Size; i++)
+            for (c = 0; c < 4; c++)
             {
-                byte b0 = block[i,0];
-                byte b1 = block[i,1];
-                byte b2 = block[i,2];
-                byte b3 = block[i,3];
-
-                block[i,0] = (byte)(GF.Multiply(b0, 2) ^ GF.Multiply(b1, 3) ^ b2 ^ b3);
-                block[i,0] = (byte)(b0 ^ GF.Multiply(b1, 2) ^ GF.Multiply(b2, 3) ^ b3);
-                block[i,0] = (byte)(b0 ^ b1 ^ GF.Multiply(b2, 2) ^ GF.Multiply(b3, 3));
-                block[i,0] = (byte)(GF.Multiply(b0, 3) ^ b1 ^ b2 ^ GF.Multiply(b3, 2));
+                a[c] = row[c];
+                h = (byte)(row[c] & 0x80); /* hi bit */
+                b[c] = (byte)(row[c] << 1);
+                if (h == 0x80)
+                    b[c] ^= 0x1b;
             }
-            return block;
+            row[0] = (byte)(b[0] ^ a[3] ^ a[2] ^ b[1] ^ a[1]);
+            row[1] = (byte)(b[1] ^ a[0] ^ a[3] ^ b[2] ^ a[2]);
+            row[2] = (byte)(b[2] ^ a[1] ^ a[0] ^ b[3] ^ a[3]);
+            row[3] = (byte)(b[3] ^ a[2] ^ a[1] ^ b[0] ^ a[0]);
+            return row;
         }
     }
     internal class Program
@@ -481,6 +457,7 @@ namespace AES
         private static byte[] plaintextBytesSliced;
         private static byte[] plaintextBytes;
         private static byte[] roundArray;
+        private static byte[] column;
         private static Block[] plaintextBlocks;
         private static Config config;
         private static KeyExpansion key;
@@ -511,7 +488,12 @@ namespace AES
         }
         private static void ConvertToPlaintextBytes()
         {
-            plaintextBytes = Encoding.UTF8.GetBytes(config.Plaintext); // UTF8, each character is represented as a byte
+            //plaintextBytes = Encoding.UTF8.GetBytes(config.Plaintext); // UTF8, each character is represented as a byte
+            plaintextBytes = new byte[]
+{
+    0x30, 0xC8, 0x1C, 0x46, 0xA3, 0x5C, 0xE4, 0x11,
+    0xE5, 0xFB, 0xC1, 0x19, 0x1A, 0x0A, 0x52, 0xEF
+};
             File.AppendAllText(
                 config.Path + config.outputFiles[0],
                 "# Plaintext in UTF8 bytes #\n" + Convert.ToHexString(plaintextBytes) + "\n\n"
@@ -528,7 +510,7 @@ namespace AES
             ConvertToPlaintextBytes();
 
             FillBlocks();
-      
+
             // For each block
             // 10 rounds !!
             for (int blockNum = 0; blockNum < numberOfBlocks; blockNum++)
@@ -537,6 +519,7 @@ namespace AES
                 // Add round key 1
                 Array.Copy(key.ExpandedKey, roundBytes, roundArray, 0, 16); // source, index, destination, length
                 plaintextBlocks[blockNum] = encrypt.AddRoundKey(plaintextBlocks[blockNum], roundArray);
+                
                 for (int round = 1; round < 10; round++) // 9 rounds
                 {
                     File.AppendAllText(config.Path + config.outputFiles[round], "# Input blocks #\n");
@@ -550,7 +533,6 @@ namespace AES
                     {
                         for (int j = 0; j < plaintextBlocks[blockNum].Size; j++)
                         {
-                            //plaintextBlocks[blockNum][i, j] = encrypt.SubByte(plaintextBlocks[blockNum][i, j]);
                             plaintextBlocks[blockNum][i, j] = GF.ApplySbox(plaintextBlocks[blockNum][i, j]);
                         }
                     }
@@ -568,9 +550,22 @@ namespace AES
                         block.WriteBlock(config.Path + config.outputFiles[round], block);
                     }
 
-
                     // Mix columns
-                    plaintextBlocks[blockNum] = encrypt.MixColumns(plaintextBlocks[blockNum]);
+                    column = new byte[4];
+
+                    for (int a = 0; a < 4; a++)
+                    {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            column[i] = plaintextBlocks[blockNum][a, i];
+                        }
+                        column = encrypt.MixColumn(column);
+
+                        for (int i = 0; i < 4; i++)
+                        {
+                            plaintextBlocks[blockNum][a, i] = column[i];
+                        }
+                    }
 
                     File.AppendAllText(config.Path + config.outputFiles[round], "# Mix columns #\n");
                     foreach (Block block in plaintextBlocks)
@@ -602,7 +597,7 @@ namespace AES
                 roundBytes += 16;
                 Array.Copy(key.ExpandedKey, roundBytes, roundArray, 0, 16);
                 plaintextBlocks[blockNum] = encrypt.AddRoundKey(plaintextBlocks[blockNum], roundArray);
-            } 
+            }
 
             foreach (Block block in plaintextBlocks)
             {
@@ -610,21 +605,34 @@ namespace AES
             }
 
             /* Test mix columns
-            byte[] test = new byte[]
-            {
-                0x09, 0x28, 0x7F, 0x47,
-                0x6F, 0x74, 0x6A, 0xBF,
-                0x2C, 0x4A, 0x62, 0x04,
-                0xDA, 0x08, 0xE3, 0xEE
+            byte[] testV = new byte[] {
+                0xdb, 0x13, 0x53, 0x45,
+                0xf2, 0x0a, 0x22, 0x5c,
+                0x01, 0x01, 0x01, 0x01,
+                0xc6, 0xc6, 0xc6, 0xc6
             };
-            Block blockFAKE = new Block(test);
-            blockFAKE = encrypt.MixColumn(blockFAKE);
+            Block blockFAKE = new Block(testV);
+            blockFAKE.WriteBlock("C:\\Users\\Tristan\\source\\repos\\REAL AES PROJECT\\AES\\Files\\test.txt", blockFAKE);
 
+            byte[] col = new byte[4];
+
+            for (int a = 0; a < 4; a++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    col[i] = blockFAKE[a, i];
+                }
+                col = encrypt.MixColumn(col);
+                for (int i = 0; i < 4; i++)
+                {
+                    blockFAKE[a, i] = col[i];
+                }
+            }
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    Console.Write(blockFAKE[i, j].ToString("X2") + " ");
+                    Console.Write(blockFAKE[j,i].ToString("X2") + " ");
                 }
                 Console.Write("\n");
             }*/
